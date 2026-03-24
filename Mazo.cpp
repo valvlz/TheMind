9a937556c129f1345cb4f12f2545f2ea8b7d3118
Juan Pablo <juanquintis@gmail.com>
2026-03-24 11:22:18 -0500

    feat(mazo): implementar barajar, control sin cartas y mejora en repartir()

--- Mazo.cpp
+++ Mazo.cpp
@@ -1,13 +1,27 @@
 #include "Mazo.h"
+#include <algorithm> //para std::shuffle
+#include <random> //para std::default_random_engine
+#include <stdexcept> //para std::runtime_error
 
 Mazo::Mazo() {
     for (int i = 1; i <= 100; i++) {
         cartas.push_back(Carta(i));
     }
+    barajar();
 }
 
 Carta Mazo::repartir() {
+    if (estaVacio()) {
+    throw std::runtime_error("El mazo está vacío"); //evitar repartir cartas si el mazo se ha quedado sin cartas
+    }
     Carta c = cartas.back();
     cartas.pop_back();
     return c;
 }
+
+void Mazo::barajar() {
+    std::random_device rd; //semilla para el generador de números aleatorios
+    std::mt19937 g(rd()); //generador de números aleatorios
+    std::shuffle(cartas.begin(), cartas.end(), g); //mezcla las cartas usando el generador
+}
+
