28e69e024d280cc98170d24e6eb0585b9138c37f
Juan Pablo <juanquintis@gmail.com>
2026-03-26 15:11:21 -0500

    feat: reimplementando guardado y cacrga completa del estado del juego

--- Juego.cpp
+++ Juego.cpp
@@ -212,46 +212,92 @@ int Juego::getEstrellas() const {
 void Juego::guardarPartida() {
     ofstream archivo("partida.txt");
 
-    if (archivo.is_open()) {
-        // Guardar jugadores
-        archivo << jugadores.size() << endl;
-        for (int i = 0; i < jugadores.size(); i++) {
-            archivo << jugadores[i].getNombre() << endl;
-        }
-
-        // Guardar estado
-        archivo << nivel << endl;
-        archivo << vidas << endl;
-
-        archivo.close();
-        cout << "Partida guardada" << endl;
+    if (!archivo.is_open()) {
+        cout << "Error al guardar\n";
+        return;
     }
+
+    // jugadores
+    archivo << jugadores.size() << endl;
+    for (auto& j : jugadores) {
+        archivo << j.getNombre() << endl;
+    }
+
+    // Guardar estado
+    archivo << nivel << endl;
+    archivo << vidas << endl;
+    archivo << estrellas << endl;
+
+     // pila central
+    archivo << pilaCentral.size() << endl;
+    for (int v : pilaCentral) {
+        archivo << v << " ";
+    }
+    archivo << endl;
+
+    // manos de jugadores
+    for (auto& j : jugadores) {
+        archivo << j.cantidadCartas() << endl;
+        for (int i = 0; i < j.cantidadCartas(); i++) {
+            archivo << j.verCarta(i).getNumero() << " ";
+        }
+        archivo << endl;
+    }
+
+    archivo.close();
+    cout << "Partida guardada correctamente\n" << endl;
 }
+
 
 // Cargar partida
 void Juego::cargarPartida() {
     ifstream archivo("partida.txt");
 
-    if (archivo.is_open()) {
-        int numJugadores;
-        archivo >> numJugadores;
-
-        jugadores.clear();
-
-        // Cargar jugadores
-        for (int i = 0; i < numJugadores; i++) {
-            string nombre;
-            archivo >> nombre;
-            jugadores.push_back(Jugador(nombre));
-        }
-
-        // Cargar estado
-        archivo >> nivel;
-        archivo >> vidas;
-
-        archivo.close();
-        cout << "Partida cargada" << endl;
-    } else {
-        cout << "No hay partida guardada" << endl;
+    if (!archivo.is_open()) {
+        cout << "No hay partida guardada\n";
+        return;
     }
+    
+    int numJugadores;
+    archivo >> numJugadores;
+
+    jugadores.clear();
+
+    // Cargar jugadores
+    for (int i = 0; i < numJugadores; i++) {
+        string nombre;
+        archivo >> nombre;
+        jugadores.push_back(Jugador(nombre));
+    }
+
+    // Cargar estado
+    archivo >> nivel;
+    archivo >> vidas;
+    archivo >> estrellas;
+
+    // pila central
+    int tamanoPila;
+    archivo >> tamanoPila;
+    pilaCentral.clear();
+
+    for (int i = 0; i < tamanoPila; i++) {
+        int valor;
+        archivo >> valor;
+        pilaCentral.push_back(valor);
+    }
+
+    // manos
+    for (auto& j : jugadores) {
+        int cantidad;
+        archivo >> cantidad;
+
+        for (int i = 0; i < cantidad; i++) {
+            int num;
+            archivo >> num;
+            j.recibirCarta(Carta(num));
+        }
+    }
+    archivo.close();
+    cout << "Partida cargada correctamente\n" << endl;
+    
 }
